29e73317a3faf7b8d8c28028eb63e5221f9c10a6
ddrrdddd <ddrrdddd>
2023-10-31 20:56:41 +0900

    [rename] WebDriverOptionConfiguration -> WebDriverConfig

--- src/main/java/com/project/config/WebDriverConfig.java
+++ src/main/java/com/project/config/WebDriverConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebDriverOptionsConfiguration {
+public class WebDriverConfig {
 
     @Bean
     public ChromeOptions chromeOptions(){
