ba11c91d93a68f6dbe986a8f20ad24f7423b0173
DDRRDDDD <youngmin4018@gmail.com>
2023-11-06 23:32:48 +0900

    [feat] MainPage main URL에 접속하는 기능 추가 / 민감한 정보를 다른YAML 파일 분리

--- src/main/java/com/project/page/MainPage.java
+++ src/main/java/com/project/page/MainPage.java
@@ -2,16 +2,22 @@ package com.project.page;
 
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 @Component
-@RequiredArgsConstructor
 public class MainPage {
 
     private final WebDriverWait webDriverWait;
+
+    private final WebDriver webDriver;
+
+    private final String MAIN_PAGE_URL;
 
     private final By loginIdInput = By.id("login_id");
 
@@ -19,6 +25,17 @@ public class MainPage {
 
     private final By loginButton = By.id("btn_login");
 
+    public MainPage(@Autowired WebDriverWait webDriverWait, @Autowired WebDriver webDriver,
+                    @Value("${dev.scraper.mainUrl}") String mainUrl){
+        this.webDriverWait = webDriverWait;
+        this.webDriver = webDriver;
+        MAIN_PAGE_URL = mainUrl;
+    }
+
+    public MainPage loadMainPage(){
+        webDriver.get(MAIN_PAGE_URL);
+        return this;
+    }
 
     public MainPage enterLoginUserIdInput(String userId){
         webDriverWait.until(visibilityOfElementLocated(loginIdInput))
