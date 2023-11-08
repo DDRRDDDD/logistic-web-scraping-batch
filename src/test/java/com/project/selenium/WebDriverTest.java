2bc563b8781995f860bcc0bad06f7053f5272d10
DDRRDDDD <youngmin4018@gmail.com>
2023-11-08 23:29:05 +0900

    [test] 웹 드라이버 태스트 코드 추가

--- src/test/java/com/project/selenium/WebDriverTest.java
+++ src/test/java/com/project/selenium/WebDriverTest.java
@@ -1,30 +1,72 @@
 package com.project.selenium;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.project.config.WebDriverConfig.WAIT_TIMEOUT_SECONDS;
+
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class WebDriverTest {
-
-    @Autowired
     private WebDriver webDriver;
+
+    @Value("${dev.scraper.mainUrl}")
+    private String mainUrl;
+
+
+    @BeforeAll
+    public static void beforeAll(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeTest(@Autowired ChromeOptions options){
+        webDriver = new ChromeDriver(options);
+        webDriver.manage().timeouts().pageLoadTimeout(WAIT_TIMEOUT_SECONDS);
+        webDriver.manage().timeouts().scriptTimeout(WAIT_TIMEOUT_SECONDS);
+    }
+
     @Test
     @DisplayName("드라이버 동작 테스트")
     public void webDriverTest1(){
         webDriver.get("https://www.google.com");
         String googleTitle = webDriver.getTitle();
+
         webDriver.get("https://www.naver.com");
         String naverTitle = webDriver.getTitle();
-        webDriver.quit();
 
         Assertions.assertEquals("GOOGLE", googleTitle.toUpperCase());
         Assertions.assertEquals("NAVER", naverTitle.toUpperCase());
+    }
+
+
+    @Test
+    @DisplayName("드라이버가 메인페이지에 정상적으로 접속이 가능한가?")
+    public void webDriverTest2(){
+        webDriver.get(mainUrl);
+        String title = webDriver.getTitle();
+
+        Assertions.assertEquals("비밀 ><", title.toUpperCase());
+    }
+
+    @AfterEach
+    public void afterTest() throws InterruptedException {
+        Thread.sleep(5000);
+        webDriver.close();
+        webDriver.quit();
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        WebDriverManager.chromedriver().quit();
     }
 }
