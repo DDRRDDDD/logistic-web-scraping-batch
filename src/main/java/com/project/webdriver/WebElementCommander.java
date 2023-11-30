02542a3760f66ea2b1a39a81a1e8556d62da50dc
DDRRDDDD <youngmin4018@gmail.com>
2023-11-30 16:18:11 +0900

    [refactor] 웹 드라이버 유틸 클래스의 분리로 전반적인 클래스 수정

--- src/main/java/com/project/webdriver/WebElementCommander.java
+++ src/main/java/com/project/webdriver/WebElementCommander.java
@@ -1,41 +1,31 @@
 package com.project.webdriver;
 
-import com.project.common.BeanManager;
+import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-
+@RequiredArgsConstructor
 public class WebElementCommander {
 
     private static final String ATTRIBUTE_VALUE = "value";
-    private final WebDriver webDriver;
-    private final WebDriverWait driverWait;
-    private final WebElement webElement;
 
-    private WebElementCommander(WebElement webElement){
-        this.webElement = webElement;
-        this.webDriver = BeanManager.getBean("chromeDriver", WebDriver.class);
-        this.driverWait = BeanManager.getBean("webDriverWait", WebDriverWait.class);
-    }
+    private final WebElement webElement;
 
     public static WebElementCommander with(WebElement webElement) {
         return new WebElementCommander(webElement);
     }
 
     public WebElement waitForValue(WebElement element, String value){
-        driverWait.until(ExpectedConditions.attributeToBe(element, ATTRIBUTE_VALUE, value));
+        WebDriverUtils.waitUntil(ExpectedConditions.attributeToBe(element, ATTRIBUTE_VALUE, value));
         return webElement;
     }
 
 
     public void selectByOptionText(String optionsText){
-        Actions actions = new Actions(webDriver);
-        actions.moveToElement(webElement).perform();
+        WebDriverUtils.getWebActions().moveToElement(webElement).perform();
 
-        WebElement webElement = webDriver.findElement(By.linkText(optionsText));
-        driverWait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
+        WebElement webElement = WebDriverUtils.findElement(By.linkText(optionsText));
+        WebDriverUtils.waitUntil(ExpectedConditions.elementToBeClickable(webElement)).click();
     }
 
     public void enterDate(String date){
