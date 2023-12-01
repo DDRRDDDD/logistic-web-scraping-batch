9c2569e86de5c4f488467dc5f1d5365d5d10817b
DDRRDDDD <youngmin4018@gmail.com>
2023-12-01 19:09:08 +0900

    [refactor] AllocationDataPopup POM 객체로 변환

---
+++ src/main/java/com/project/page/object/AllocationDataPopup.java
@@ -0,0 +1,54 @@
+package com.project.page.object;
+
+import com.project.page.Page;
+import com.project.webdriver.WebDriverUtils;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Slf4j
+@Page(pageScope=SCOPE_PROTOTYPE)
+public class AllocationDataPopup {
+
+    @FindBy(xpath = "//*[@id=\"cargoDetailView01\"]/table/tbody/tr/td[1]")
+    private List<WebElement> dataKeys;
+
+    @FindBy(xpath = "//*[@id=\"cargoDetailView01\"]/table/tbody/tr/td[2]")
+    private List<WebElement> dataValues;
+
+
+    @PostConstruct
+    public void openPopup(){
+        WebDriverUtils.switchToWindow();
+    }
+
+    public Map<String, String> extractAllocationData(){
+        return perform(IntStream.range(0, dataKeys.size())
+                    .boxed()
+                    .collect(Collectors.toMap(
+                            index -> dataKeys.get(index).getText(),
+                            index -> dataValues.get(index).getText()
+                        )
+                    )
+        );
+    }
+
+    public <T> T perform(T result){
+        try{
+            return result;
+        } finally {
+            WebDriverUtils.closeCurrentWindow();
+            WebDriverUtils.switchToWindow();
+        }
+    }
+
+
+}
