7309ad29e06c6cf302e10cfbdd44e69fbd86ba41
DDRRDDDD <youngmin4018@gmail.com>
2023-12-02 17:35:40 +0900

    [refactor] switchToWindow() 메서드가 창이 열고 닫힐 때 사용되도록 수정

--- src/main/java/com/project/webdriver/WebDriverUtils.java
+++ src/main/java/com/project/webdriver/WebDriverUtils.java
@@ -1,15 +1,14 @@
 package com.project.webdriver;
 
 import lombok.NoArgsConstructor;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import static lombok.AccessLevel.PRIVATE;
@@ -18,6 +17,9 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access = PRIVATE)
 public class WebDriverUtils {
 
+    public static <V> V waitUntil(Function<? super WebDriver, V> condition) {
+        return WebDriverProvider.getDriverWait().until(condition);
+    }
     public static  WebElement findElement(By by){
         return WebDriverProvider.getWebDriver().findElement(by);
     }
@@ -25,8 +27,34 @@ public class WebDriverUtils {
     public static Actions getWebActions(){
         return new Actions(WebDriverProvider.getWebDriver());
     }
-    public static <V> V waitUntil(Function<? super WebDriver, V> condition) {
-        return WebDriverProvider.getDriverWait().until(condition);
+
+    public static JavascriptExecutor getJavascriptExecutor(){
+        return (JavascriptExecutor) WebDriverProvider.getWebDriver();
+    }
+
+    public static void switchToWindow(){
+        WebDriver webDriver = WebDriverProvider.getWebDriver();
+        String currentWindow = getCurrentWindowHandle();
+
+        webDriver.getWindowHandles().stream()
+                .filter(window -> !window.equals(currentWindow))
+                .findFirst()
+                .ifPresent(webDriver.switchTo()::window);
+
+    }
+
+
+    public static void closeCurrentWindow(){
+        WebDriverProvider.getWebDriver().close();
+    }
+
+
+    private static String getCurrentWindowHandle(){
+        try{
+            return WebDriverProvider.getWebDriver().getWindowHandle();
+        }catch(NoSuchWindowException exception){
+            return "";
+        }
     }
 
 
