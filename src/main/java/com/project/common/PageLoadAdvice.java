2ca364b3acba7d3c25e67c6f301c7aa8c5d977a3
DDRRDDDD <youngmin4018@gmail.com>
2023-12-03 20:00:42 +0900

    [refactor] 페이지가 전부 로딩이 되었는지 파악하는 Advice 추가

---
+++ src/main/java/com/project/common/PageLoadAdvice.java
@@ -0,0 +1,50 @@
+package com.project.common;
+
+import com.project.webdriver.WebDriverUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang3.BooleanUtils;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@Slf4j
+@Component
+public class PageLoadAdvice implements MethodInterceptor {
+
+    @Nullable @Override
+    public Object invoke(@Nonnull MethodInvocation invocation) throws Throwable {
+        WebDriverUtils.waitUntil(expectedCondition());
+        log.info("Page load complete. Proceeding with method invocation.");
+        return invocation.proceed();
+    }
+
+    private ExpectedCondition<Boolean> expectedCondition(){
+        return (webDriver) -> {
+            try{
+                return Boolean.logicalAnd(isJQueryLoaded(), isJSLoaded());
+            }catch(Exception exception){
+                return Boolean.TRUE;
+            }
+        };
+    }
+
+
+    private boolean isJQueryLoaded() {
+        return WebDriverUtils.getJavascriptExecutor()
+                .executeScript("return jQuery.active === 0")
+                .toString()
+                .equals(BooleanUtils.TRUE);
+    }
+
+    private boolean isJSLoaded() {
+        return WebDriverUtils.getJavascriptExecutor()
+                .executeScript("return document.readyState")
+                .toString()
+                .equals("complete");
+    }
+
+}
--- src/main/java/com/project/common/PageObjectInitializer.java
+++ src/main/java/com/project/common/PageObjectInitializer.java
@@ -1,13 +1,15 @@
-package com.project.page;
+package com.project.common;
 
+import com.project.page.Page;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.aop.Advice;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
-
 
 
 @Slf4j
@@ -15,18 +17,28 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PageObjectInitializer implements BeanPostProcessor {
 
-    private final ElementLocatorFactory elementLocatorFactory;
+    private final Advice pageLoadAdvice;
+
+    private final ElementLocatorFactory ajaxElementLocatorFactory;
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if(bean.getClass().isAnnotationPresent(Page.class)){
             pageElementsInitialization(bean, beanName);
+            return getPageObjectProxy(bean);
         }
         return bean;
     }
 
+    private Object getPageObjectProxy(Object bean){
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvice(pageLoadAdvice);
+        return proxyFactory.getProxy();
+    }
+
+
     private void pageElementsInitialization(Object bean, String beanName){
-        PageFactory.initElements(elementLocatorFactory, bean);
+        PageFactory.initElements(ajaxElementLocatorFactory, bean);
         log.info("Initialize Page Object : {}", beanName);
     }
 }
