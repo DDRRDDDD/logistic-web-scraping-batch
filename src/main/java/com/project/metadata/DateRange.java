2e681e1f87f415a6d8bc074a8c607d4e01bc4014
DDRRDDDD <youngmin4018@gmail.com>
2023-12-04 15:39:02 +0900

    [feat] 하루 단위 날짜 메서드 생성

--- src/main/java/com/project/metadata/DateRange.java
+++ src/main/java/com/project/metadata/DateRange.java
@@ -16,9 +16,14 @@ public class DateRange {
     private final String startDateOfMonth;
     private final String endDateOfMonth;
 
-    public static DateRange of(int year, int month){
+    public static DateRange ofMonth(int year, int month) {
         LocalDate startDateOfMonth = LocalDate.of(year, month, FIRST_DAY_OF_MONTH);
         LocalDate endDateOfMonth = LocalDate.of(year, month, startDateOfMonth.lengthOfMonth());
         return new DateRange(startDateOfMonth.toString(), endDateOfMonth.toString());
+    }
+
+    public static DateRange ofDay(int year, int month, int day) {
+        LocalDate dateOfMonth = LocalDate.of(year, month, day);
+        return new DateRange(dateOfMonth.toString(), dateOfMonth.toString());
     }
 }
