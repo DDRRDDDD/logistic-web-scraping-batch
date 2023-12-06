12beceb89eb59f5aa701508f46d85ade902a6991
DDRRDDDD <youngmin4018@gmail.com>
2023-12-06 15:57:03 +0900

    [feat] 연단위(1/1 ~ 12/31)의 날짜 범위를 반환하는 메서드 생성

--- src/main/java/com/project/metadata/DateRange.java
+++ src/main/java/com/project/metadata/DateRange.java
@@ -11,11 +11,23 @@ import static lombok.AccessLevel.PRIVATE;
 @RequiredArgsConstructor(access = PRIVATE)
 public class DateRange {
 
+    // 1월 1일
+    private static final int FIRST_MONTH_OF_YEAR = 1;
     private static final int FIRST_DAY_OF_MONTH = 1;
+
+    // 12월 31일
+    private static final int LAST_MONTH_OF_YEAR = 12;
+    private static final int LAST_DAY_OF_MONTH = 31;
 
     private final String startDateOfMonth;
     private final String endDateOfMonth;
 
+
+    public static DateRange ofYear(int year){
+        LocalDate startDateOfMonth = LocalDate.of(year, FIRST_MONTH_OF_YEAR, FIRST_DAY_OF_MONTH);
+        LocalDate endDateOfMonth = LocalDate.of(year, LAST_MONTH_OF_YEAR, LAST_DAY_OF_MONTH);
+        return new DateRange(startDateOfMonth.toString(), endDateOfMonth.toString());
+    }
     public static DateRange ofMonth(int year, int month) {
         LocalDate startDateOfMonth = LocalDate.of(year, month, FIRST_DAY_OF_MONTH);
         LocalDate endDateOfMonth = LocalDate.of(year, month, startDateOfMonth.lengthOfMonth());
