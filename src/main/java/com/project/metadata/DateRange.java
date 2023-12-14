51b6ef7556201a4d4d72ed42b261ca488b27fef8
DDRRDDDD <youngmin4018@gmail.com>
2023-12-14 18:28:16 +0900

    [refactor] ofThisYear() 이번 년도 메서드 추가 / DateRange 테스트 케이스 추가

--- src/main/java/com/project/metadata/DateRange.java
+++ src/main/java/com/project/metadata/DateRange.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.Year;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -28,19 +29,29 @@ public class DateRange {
         LocalDate endDateOfMonth = LocalDate.of(year, LAST_MONTH_OF_YEAR, LAST_DAY_OF_MONTH);
         return new DateRange(startDateOfMonth.toString(), endDateOfMonth.toString());
     }
+
+
+    public static DateRange ofThisYear(){
+        return ofYear(Year.now().getValue());
+    }
+
+
     public static DateRange ofMonth(int year, int month) {
         LocalDate startDateOfMonth = LocalDate.of(year, month, FIRST_DAY_OF_MONTH);
         LocalDate endDateOfMonth = LocalDate.of(year, month, startDateOfMonth.lengthOfMonth());
         return new DateRange(startDateOfMonth.toString(), endDateOfMonth.toString());
     }
 
+
     public static DateRange ofDay(int year, int month, int day) {
         LocalDate dateOfMonth = LocalDate.of(year, month, day);
         return new DateRange(dateOfMonth.toString(), dateOfMonth.toString());
     }
 
+
     public static DateRange ofToday(){
         LocalDate currentDate = LocalDate.now();
         return ofDay(currentDate.getYear(), currentDate.getMonthValue(), currentDate.getDayOfMonth());
     }
+
 }
--- src/test/java/com/project/selenium/DateRangeTest.java
+++ src/test/java/com/project/selenium/DateRangeTest.java
@@ -10,10 +10,50 @@ public class DateRangeTest {
     @Test
     @DisplayName("시작일과 마지막일이 잘 출력이 되는가?")
     public void test1(){
-        DateRange date = DateRange.ofMonth(2023, 2);
+        DateRange dateRange = DateRange.ofMonth(2023, 2);
 
-        Assertions.assertEquals("2023-02-01", date.getStartDateOfMonth());
-        Assertions.assertEquals("2023-02-28", date.getEndDateOfMonth());
+        Assertions.assertEquals("2023-02-01", dateRange.getStartDateOfMonth());
+        Assertions.assertEquals("2023-02-28", dateRange.getEndDateOfMonth());
+    }
+
+    @Test
+    @DisplayName("입력 년도의 (1월 1일 / 12월 31일) 출력 테스트")
+    public void dateRangeOfYearTest(){
+        DateRange dateRange = DateRange.ofYear(2023);
+
+        Assertions.assertEquals("2023-01-01", dateRange.getStartDateOfMonth());
+        Assertions.assertEquals("2023-12-31", dateRange.getEndDateOfMonth());
+    }
+
+    @Test
+    @DisplayName("이번 년도의 (1월 1일 / 12월 31일) 출력 테스트")
+    public void dateRangeOfThisYearTest(){
+        DateRange dateRange = DateRange.ofThisYear();
+
+        Assertions.assertEquals("2023-01-01", dateRange.getStartDateOfMonth());
+        Assertions.assertEquals("2023-12-31", dateRange.getEndDateOfMonth());
+    }
+
+    @Test
+    @DisplayName("하루 출력 테스트")
+    public void dateRangeOfMonthTest(){
+        DateRange dateRange = DateRange.ofDay(2023, 1, 1);
+
+        Assertions.assertEquals("2023-01-01", dateRange.getStartDateOfMonth());
+        Assertions.assertEquals("2023-01-01", dateRange.getEndDateOfMonth());
+    }
+
+    /**
+     * 해당 테스트 코드는 2023.12.14 기준으로 작성되었습니다.
+     * 테스트를 다시 실행하려면 기대값을 수정하세요.
+     */
+    @Test
+    @DisplayName("오늘 날짜 출력 테스트")
+    public void dateRangeOfTodayTest(){
+        DateRange dateRange = DateRange.ofToday();
+
+        Assertions.assertEquals("2023-12-14", dateRange.getStartDateOfMonth());
+        Assertions.assertEquals("2023-12-14", dateRange.getEndDateOfMonth());
     }
 
 }
