8449e6b4cc40759bf561c8ba4c5406cdb6b5b9d5
DDRRDDDD <youngmin4018@gmail.com>
2024-01-07 17:41:41 +0900

    [feat] 임시 ChunckListener 생성 및 적용

--- src/main/java/com/project/batch/StepConfig.java
+++ src/main/java/com/project/batch/StepConfig.java
@@ -1,11 +1,13 @@
 package com.project.batch;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ChunkListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Map;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class StepConfig {
@@ -25,16 +28,19 @@ public class StepConfig {
 
     private final PlatformTransactionManager transactionManager;
 
+    private final ChunkListener chunkListener;
+
     // jobparameter로 수정할거임
     private final int chunkSize = 10;
 
 
     @JobScope
     @Bean(DAILY_ALLOCATION_STEP)
-    public Step dailyAllocationStep(@Qualifier("dailyAllocationItemScraper") ItemReader<Map<String, String>> itemReader,
-                                     ItemWriter<Map<String, String>> itemWriter) {
+    public Step dailyAllocationStep(@Qualifier("dailyAllocationItemScraper") ItemStreamReader<Map<String, String>> itemReader,
+                                    ItemWriter<Map<String, String>> itemWriter) {
         return new StepBuilder(DAILY_ALLOCATION_STEP, jobRepository)
                 .<Map<String, String>, Map<String, String>>chunk(chunkSize, transactionManager)
+                .listener(chunkListener)
                 .reader(itemReader)
                 .writer(itemWriter)
                 .build();
@@ -43,14 +49,14 @@ public class StepConfig {
 
     @JobScope
     @Bean(YEARLY_ALLOCATION_STEP)
-    public Step yearlyAllocationStep(@Qualifier("yearlyAllocationItemScraper") ItemReader<Map<String, String>> itemReader,
+    public Step yearlyAllocationStep(@Qualifier("yearlyAllocationItemScraper") ItemStreamReader<Map<String, String>> itemReader,
                                      ItemWriter<Map<String, String>> itemWriter) {
         return new StepBuilder(YEARLY_ALLOCATION_STEP, jobRepository)
                 .<Map<String, String>, Map<String, String>>chunk(chunkSize, transactionManager)
+                .listener(chunkListener)
                 .reader(itemReader)
                 .writer(itemWriter)
                 .build();
     }
-
 
 }
