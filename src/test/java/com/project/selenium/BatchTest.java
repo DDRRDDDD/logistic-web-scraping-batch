ca1b4eb4eb6364ce49649cd1be0689bdb46d6e27
DDRRDDDD <youngmin4018@gmail.com>
2024-01-07 17:50:35 +0900

    [feat] 기존 배치 테스트에 데이터소스 테스트를 구분하고 순수 배치 테스트 코드로 재배치

--- src/test/java/com/project/selenium/BatchTest.java
+++ src/test/java/com/project/selenium/BatchTest.java
@@ -1,48 +1,53 @@
 package com.project.selenium;
 
-import com.project.scraper.AllocationItemScraperBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.sql.DataSource;
+import static com.project.batch.JobConfig.DAILY_ALLOCATION_JOB;
+import static com.project.batch.JobConfig.YEARLY_ALLOCATION_JOB;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
+@SpringBatchTest
 public class BatchTest {
 
-    private final Logger log = LoggerFactory.getLogger(BatchTest.class);
-
-    private final ApplicationContext context;
-
-    private final DataSource dataSource;
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Autowired
-    public BatchTest(ApplicationContext context, DataSource dataSource){
-        this.context = context;
-        this.dataSource = dataSource;
+    @Qualifier(DAILY_ALLOCATION_JOB)
+    private Job dailyAllocationJob;
+
+    @Autowired
+    @Qualifier(YEARLY_ALLOCATION_JOB)
+    private Job yearlyAllocationJob;
+
+
+    @Test
+    @DisplayName("일일 배차내역 조회 스크래퍼 테스트")
+    public void dailyScraperTest() throws Exception {
+        jobLauncherTestUtils.setJob(this.dailyAllocationJob);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
     }
 
     @Test
-    @DisplayName("배차 조회 스크래퍼 빌더 클래스 테스트")
-    public void allocationItemScraperBuilderTest(){
-        AllocationItemScraperBuilder builder1 = context.getBean(AllocationItemScraperBuilder.class);
-        AllocationItemScraperBuilder builder2 = context.getBean(AllocationItemScraperBuilder.class);
+    @DisplayName("연간 배차내역 조회 스크래퍼 테스트")
+    public void yearlyScraperTest() throws Exception {
+        jobLauncherTestUtils.setJob(this.yearlyAllocationJob);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 
-        Assertions.assertNotEquals(builder1, builder2);
+        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
     }
 
-    @Test
-    @DisplayName("데이터 소스의 구현 클래스 확인")
-    public void defaultDataSourceTest(){
-        String className = dataSource.getClass().getSimpleName();
-        log.info("DataSource class >>> {}", className);
-    }
+
 }
