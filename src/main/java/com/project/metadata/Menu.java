38a9fa7b21a685a25e24cacc4ffd25b6202541d8
DDRRDDDD <youngmin4018@gmail.com>
2024-01-15 14:57:14 +0900

    [refactor] 맴버 클래스로 존재하던 MenuFactory와 JobParameter를 받는 날짜 범위 클래스를 MetaDataConfig로 합침

--- src/main/java/com/project/metadata/Menu.java
+++ src/main/java/com/project/metadata/Menu.java
@@ -1,26 +1,13 @@
 package com.project.metadata;
 
-import com.project.page.object.AllocationPage;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
-@RequiredArgsConstructor(access= AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class Menu<T> {
 
     private final String option;
     private final Class<T> pageClass;
-
-    @Configuration
-    static class MenuFactory {
-        @Bean
-        public Menu<AllocationPage> allocationPageMenu(){
-            return new Menu<>("배차내역조회", AllocationPage.class);
-        }
-
-    }
 
 }
---
+++ src/main/java/com/project/metadata/MetaDataConfig.java
@@ -0,0 +1,30 @@
+package com.project.metadata;
+
+import com.project.page.object.AllocationPage;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetaDataConfig {
+
+    @Bean
+    public Menu<AllocationPage> allocationPageMenu(){
+        return new Menu<>("배차내역조회", AllocationPage.class);
+    }
+
+
+    @Bean
+    @JobScope
+    public DateRange dailyDateRange(@Value("#{jobParameters[currentDate]}") String currentDate){
+        return DateRange.ofYesterday(currentDate);
+    }
+
+
+    @Bean
+    @JobScope
+    public DateRange yearlyDateRange(@Value("#{jobParameters[currentDate]}") String currentDate){
+        return DateRange.ofYear(currentDate);
+    }
+}
