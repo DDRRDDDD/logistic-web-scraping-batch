48cfdaa74c5fdf953499963482737535733a928e
DDRRDDDD <youngmin4018@gmail.com>
2024-03-21 18:42:50 +0900

    [rename] object 패키지로 이동
    딱히 나눌 필요가 없어서..?

--- src/main/java/com/project/page/object/BasePage.java
+++ src/main/java/com/project/page/object/BasePage.java
@@ -1,11 +1,12 @@
-package com.project.page.base;
+package com.project.page.object;
 
 
 import com.project.common.PageBeanFactory;
+import com.project.page.object.Header;
 
 public class BasePage {
 
-    public Header toHeader(){
+    public Header toHeader() {
         return PageBeanFactory.getPage(Header.class);
     }
 
---
+++ src/main/java/com/project/page/object/Header.java
@@ -0,0 +1,40 @@
+package com.project.page.object;
+
+import com.project.common.PageBeanFactory;
+import com.project.metadata.Menu;
+import com.project.page.Page;
+import com.project.webdriver.WebControlAgent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+@Page
+public class Header {
+
+    @FindBy(id = "btn_main")
+    private WebElement mainLogoAnchor;
+
+    @FindBy(id = "btn_header_orderStatus")
+    private WebElement myPageLinkAnchor;
+
+    public MainPage goToMainPage() {
+        mainLogoAnchor.click();
+        return PageBeanFactory.getPage(MainPage.class);
+    }
+
+    // 마이페이지 메뉴를 선택하여 해당 페이지로 이동하는 메서드
+    public <T> T goToPageByMyPageMenu(Menu<T> menu) {
+        WebControlAgent.moveBy(myPageLinkAnchor);
+
+        By menuOptionLocator = By.linkText(menu.getOption());
+        ExpectedCondition<WebElement> expectedCondition =
+                ExpectedConditions.elementToBeClickable(menuOptionLocator);
+
+        WebControlAgent.waitUntil(expectedCondition);
+        return PageBeanFactory.getPage(menu.getPageClass());
+    }
+
+}
+
