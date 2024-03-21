009edf3ca240e296988b653629571efae8142e87
DDRRDDDD <youngmin4018@gmail.com>
2024-03-21 18:46:42 +0900

    [feat] WebControlAgent 생성
    기존의 WebDriverUtils, WebElementCommander 클래스의 역할을 합침, WebControlAgent가 Driver를 반환하지 않고 직접 Driver를 제어할 수 있게 설계함

---
+++ src/main/java/com/project/webdriver/WebControlAgent.java
@@ -0,0 +1,91 @@
+package com.project.webdriver;
+
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class WebControlAgent {
+
+    public static WebElement findElement(By by) {
+        return WebDriverProvider.getWebDriver().findElement(by);
+    }
+
+    public static <V> V waitUntil(Function<? super WebDriver, V> condition) {
+        return WebDriverProvider.getWebDriverWait().until(condition);
+    }
+
+    public static void moveBy(WebElement element) {
+        WebDriverProvider.getWebActions().moveToElement(element).perform();
+    }
+
+    public static void waitUntilPageLoad(String expectedState) {
+        boolean isPageLoaded = WebDriverProvider.getJavascriptExecutor()
+                .executeScript("return document.readyState")
+                .toString().equals(expectedState);
+
+        waitUntil(webDriver -> isPageLoaded);
+    }
+
+    public static void switchToWindow() {
+        WebDriver webDriver = WebDriverProvider.getWebDriver();
+        String currentWindow = getCurrentWindowHandle();
+
+        webDriver.getWindowHandles().stream()
+                .filter(window -> !window.equals(currentWindow))
+                .findFirst()
+                .ifPresent(webDriver.switchTo()::window);
+    }
+
+    public static void closeCurrentWindow() {
+        WebDriverProvider.getWebDriver().close();
+    }
+
+    private static String getCurrentWindowHandle() {
+        try{
+            return WebDriverProvider.getWebDriver().getWindowHandle();
+        }catch(NoSuchWindowException exception){
+            return StringUtils.EMPTY;
+        }
+    }
+
+
+    @Component
+    private static class WebDriverProvider implements ApplicationContextAware {
+
+        private static ApplicationContext context;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            context = applicationContext;
+        }
+
+        private static WebDriver getWebDriver() {
+            return context.getBean(RemoteWebDriver.class);
+        }
+
+        private static JavascriptExecutor getJavascriptExecutor() {
+            return context.getBean(RemoteWebDriver.class);
+        }
+
+        private static WebDriverWait getWebDriverWait() {
+            return context.getBean(WebDriverWait.class);
+        }
+
+        private static Actions getWebActions() {
+            WebDriver actionsWebDriver = getWebDriver();
+            return new Actions(actionsWebDriver);
+        }
+    }
+}
