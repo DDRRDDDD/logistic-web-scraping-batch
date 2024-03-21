12fa78274d51d65306ada668e86ad5a352fbf1a7
DDRRDDDD <youngmin4018@gmail.com>
2024-03-21 18:58:56 +0900

    [refactor] 설정 파일을 수정
    - Options Bean 들의 추상화 단계를 올림
    - 페이지 로드 전략을 NORMAL로 변경 (스크래퍼 안정성)
    - WebDriverManager Bean은 추후 Driver의 알 수 없는 오류로 인해 빠른 교환 할 수 있게 driver 종류를 모아뒀음 (환경변수로 드라이버의 종류를 주입 받을 예정)
    - WebControlAgent 에서 많은 Driver 형태로 분리하기 위해 추상화 단계를 낮춤

--- src/main/java/com/project/webdriver/SeleniumConfig.java
+++ src/main/java/com/project/webdriver/SeleniumConfig.java
@@ -2,16 +2,19 @@ package com.project.webdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
 import java.time.Duration;
 
@@ -22,11 +25,10 @@ public class SeleniumConfig {
     private static final int WAIT_TIMEOUT_SECONDS = 10;
     public static final Duration DRIVER_WAIT_DURATION = Duration.ofSeconds(WAIT_TIMEOUT_SECONDS);
 
-
     @Bean
-    public ChromeOptions chromeOptions(){
+    public Capabilities chromeOptions() {
         ChromeOptions options = new ChromeOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER)
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL)
                 .addArguments("--incognito")
                 .addArguments("--headless")
                 .addArguments("--remote-allow-origins=*")
@@ -35,43 +37,43 @@ public class SeleniumConfig {
     }
 
     @Bean
-    public FirefoxOptions firefoxOptions(){
+    public Capabilities firefoxOptions() {
         FirefoxOptions options = new FirefoxOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER)
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL)
                 .addArguments("-private")
                 .addArguments("-headless")
                 .addPreference("dom.webnotifications.enabled", false);
         return options;
     }
 
-
-
-    @Bean(destroyMethod="quit")
-    public WebDriverManager firefoxDriverManager(){
+    /**
+     * 추후 드라이버 이름을 환경변수로 받아 실행시킬 계획
+     * ex. firefoxdriver, firefoxOptions 부분을 파라미터로 받을 예정
+     * */
+    @Bean
+    public WebDriverManager webDriverManager() {
         return WebDriverManager.firefoxdriver()
+                .capabilities(firefoxOptions())
                 .remoteAddress("http://172.20.3.16:4444/wd/hub");
     }
-
-    /**
-    * 일시적으로 파이어폭스 브라우저를 사용하기로함!!!
-     */
     @Bean
-    public WebDriver firefoxDriver(){
-        return firefoxDriverManager()
-                .capabilities(firefoxOptions())
-                .create();
+    public RemoteWebDriver webDriver() {
+        WebDriver webDriver = webDriverManager().create();
+        Assert.isTrue(webDriver instanceof RemoteWebDriver, "WebDriverManager 구성에 문제가 있을 수 있습니다.");
+        return (RemoteWebDriver) webDriver;
     }
 
     @Bean
-    public WebDriverWait webDriverWait(WebDriver webDriver){
+    public WebDriverWait webDriverWait(WebDriver webDriver) {
         WebDriverWait driverWait = new WebDriverWait(webDriver, DRIVER_WAIT_DURATION);
         driverWait.ignoring(NoSuchElementException.class);
         return driverWait;
     }
 
     @Bean
-    public ElementLocatorFactory ajaxElementLocatorFactory(WebDriver webDriver){
+    public ElementLocatorFactory ajaxElementLocatorFactory(WebDriver webDriver) {
         return new AjaxElementLocatorFactory(webDriver, WAIT_TIMEOUT_SECONDS);
     }
+
 
 }
