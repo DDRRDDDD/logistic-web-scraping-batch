902c87cf1003277db70251d6040281d924b5fb48
DDRRDDDD <youngmin4018@gmail.com>
2024-03-22 18:44:26 +0900

    [feat] getPage 메서드에 Assert 추가
    메서드의 역할을 명확하게 하기 위해서 검증 코드 추가

--- src/main/java/com/project/common/PageBeanFactory.java
+++ src/main/java/com/project/common/PageBeanFactory.java
@@ -1,11 +1,13 @@
 package com.project.common;
 
+import com.project.page.Page;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -13,9 +15,9 @@ import static lombok.AccessLevel.PRIVATE;
 public class PageBeanFactory {
 
     public static <T> T getPage(Class<T> pageClass){
+        Assert.isTrue(pageClass.isAnnotationPresent(Page.class), "This class is not registered as a page.");
         return ApplicationContextProvider.getContext().getBean(pageClass);
     }
-
 
     @Component
     private static class ApplicationContextProvider implements ApplicationContextAware {
