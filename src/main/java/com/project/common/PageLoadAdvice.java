4468235c2627e18cf910ad3d4b72c13a72191784
DDRRDDDD <youngmin4018@gmail.com>
2024-03-26 18:29:33 +0900

    [refactor] 유틸 클래스의 사용을 삭제하고 webDriver를 직접 사용하여 로직을 다시 구현함
    - WebControlAgent 클래스의 제약사항 추가로 코드 변경

--- src/main/java/com/project/common/PageLoadAdvice.java
+++ src/main/java/com/project/common/PageLoadAdvice.java
@@ -1,23 +1,36 @@
 package com.project.common;
 
-import com.project.webdriver.WebControlAgent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class PageLoadAdvice implements MethodInterceptor {
 
     private static final String COMPLETE_READY_STATE = "complete";
 
+
+    private final RemoteWebDriver webDriver;
+
+    private final WebDriverWait webDriverWait;
+
     @Override
     public Object invoke(@Nonnull MethodInvocation invocation) throws Throwable {
-        WebControlAgent.waitUntilPageLoad(COMPLETE_READY_STATE);
+        webDriverWait.until(unusedWebDriver -> isPageLoaded());
         log.info("Page load complete. Proceeding with method : {}", invocation.getMethod().getName());
         return invocation.proceed();
+    }
+
+    public boolean isPageLoaded() {
+        return webDriver.executeScript("return document.readyState")
+                .toString().equals(COMPLETE_READY_STATE);
     }
 }
